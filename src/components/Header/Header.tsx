9c708fc5a2a00df50cc9ad3d24e7407107df9504
Kath Veremeichyk <kateiiverem@gmail.com>
2022-07-13 16:53:29 +0300

    feature/US-1-create-UI. Add data & refactor of style

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,47 @@
 import { commonActions } from '../../store/reducers/common';
-import { useAppDispatch } from '../../hooks/redux';
+import { useAppDispatch, useAppSelector } from '../../hooks/redux';
 
 const Header = () => {
   const dispatch = useAppDispatch();
+  const { currencies } = useAppSelector((state) => state.currency);
+
+  const priceNum = currencies.map((currency) => +currency.priceUsd);
+  // const maxPriceArr = [];
+  //
+  // priceNum.forEach((price) => {
+  //   const maxObj = currencies.reduce((prev, cur) => (+cur.priceUsd > +prev.priceUsd ? cur : prev), { priceUsd: -Infinity });
+  //
+  // });
+  //
+  // // если нужен индекс элемента массива
+  // const maxIndex = currencies.reduce((acc, curr, i) => (currencies[acc].priceUsd > curr.priceUsd ? acc : i), 0);
+
+  // console.log(maxObj, maxIndex);
+
+  let max1 = 0;
+  let max2 = 0;
+  let max3 = 0;
+  const name = [];
+
+  if (priceNum.length > 2) {
+    currencies.forEach((currency) => {
+      if (+currency.priceUsd > max1) {
+        max3 = max2;
+        max2 = max1;
+        max1 = currency.priceUsd;
+        name.push(currency.name);
+      } else if (+currency.priceUsd > max2) {
+        max3 = max2;
+        max2 = currency.priceUsd;
+        name.push(currency.name);
+      } else if (+currency.priceUsd > max3) {
+        max3 = currency.priceUsd;
+        name.push(currency.name);
+      }
+    });
+  }
+
+  console.log(max1, max2, max3, name);
 
   return (
     <header className="header">
